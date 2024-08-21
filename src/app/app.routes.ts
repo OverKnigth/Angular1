10ee46c6be67227b240dbf1c26d9ec690b227662
OverKnigth <stalin.m0012@gmail.com>
2024-08-21 13:16:50 -0500

    act

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,8 @@ import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 import { VideojuegosComponent } from './pages/videojuegos/videojuegos.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
+    { path: '', component: HomeComponent },
     { path: 'videojuegos', component: VideojuegosComponent },
     { path: 'nosotros', component: NosotrosComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: '**', component: VideojuegosComponent }
 ];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -4,7 +4,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, NavbarComponent, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
